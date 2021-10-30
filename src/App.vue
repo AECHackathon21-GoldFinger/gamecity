8a027da3ca4477c7533bc930293802d3ab2f8712
Ryan Hughes <rhu@axisarch.tech>
2021-10-30 19:55:18 +0200

    Update UI.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { ref } from "vue";
+import Topbar from './components/Topbar.vue'
 </script>
 
 <template>
   <div>
-    <header class="bg-white shadow-lg select-none cursor-pointer">
-      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-extrabold leading-tight text-indigo-500">Gamecity</h1>
-      </div>
-    </header>
+    <Topbar/>
     <main>
       <router-view />
     </main>
